ec6ee17569b6e9fa9a12922308289657f53ff03f
Georgy Buranov <gburanov@gmail.com>
2016-04-09 22:58:59 +0200

    Start execution

--- main.go
+++ main.go
@@ -4,12 +4,18 @@ import (
   "fmt"
 )
 
+func printTheShit(val interface{}) {
+  fmt.Println(val)
+}
+
 func showStats(arr []int) {
   fmt.Println("Size", len(arr))
   fmt.Println("Capacity", cap(arr))
 }
 
 func main() {
+  printTheShit("Start execution")
+
   arr := []int{1, 2}
   showStats(arr)
   arr = append(arr, 3)
