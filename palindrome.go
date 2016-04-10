2bd50e21b3e6c78460df468bc55e40176dac5b86
Georgy Buranov <gburanov@gmail.com>
2016-04-10 22:07:49 +0200

    initial palindromes impl

--- palindrome.go
+++ palindrome.go
@@ -1,10 +1,29 @@
 package main
 
-
 func CheckIfPalindrome(str string) bool {
+  len := len(str)
+  if (len < 2) {
+    return false
+  }
+
+  for i := 0; i < len; i++ {
+    if str[i] != str[len - i - 1] {
+      return false
+    }
+  }
   return true
 }
 
-func CountPalindromes(stt string) int {
-  return 0
+func CountPalindromes(str string) []string {
+  len := len(str)
+  palindromes := []string{}
+  for start := 0; start < len; start++ {
+    for finish := len-1; finish > start; finish-- {
+      check_str := str[start:finish]
+      if (CheckIfPalindrome(check_str)) {
+        palindromes = append(palindromes, check_str)
+      }
+    }
+  }
+  return palindromes
 }
--- palindrome_test.go
+++ palindrome_test.go
@@ -5,11 +5,23 @@ import (
 )
 
 func TestPalindromes(t *testing.T) {
+  if CheckIfPalindrome("a") == true {
+    t.Fatalf("a")
+  }
+
   if CheckIfPalindrome("abba") == false {
     t.Fatalf("abba")
   }
 
   if CheckIfPalindrome("abbat") == true {
     t.Fatalf("abbat")
+  }
+}
+
+func TestPalindromesCount(t *testing.T) {
+  palindromes := CountPalindromes("barbarabar")
+  t.Log(palindromes)
+  if (len(palindromes) != 3) {
+    t.Fatalf("Shoud be 3 but %d", len(palindromes))
   }
 }
